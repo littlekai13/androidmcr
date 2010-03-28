cd8004b3fac2dc726e6e8c620c8827229c81ecd1
adriennefelt <adriennefelt@882c2ea8-2e7f-1d65-5f10-008cdebc5309>
2010-03-28 23:11:07 +0000

    adding basic gui

---
+++ trunk/MCR/src/edu/berkeley/sec/mcr/GUI.java
@@ -0,0 +1,14 @@
+package edu.berkeley.sec.mcr;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GUI extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
