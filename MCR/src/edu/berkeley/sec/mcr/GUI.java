677df9fa2742c5656e53050fbc1e9383b065b645
adriennefelt <adriennefelt@882c2ea8-2e7f-1d65-5f10-008cdebc5309>
2010-03-30 19:59:39 +0000

    successfully grab photos from a photo picker

--- MCR/src/edu/berkeley/sec/mcr/GUI.java
+++ MCR/src/edu/berkeley/sec/mcr/GUI.java
@@ -1,8 +1,13 @@
 package edu.berkeley.sec.mcr;
 
+import java.io.FileNotFoundException;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class GUI extends Activity {
@@ -13,9 +18,26 @@ public class GUI extends Activity {
         setContentView(R.layout.main);
     }
     
-    public void sayHello(View v) {
+    /*
+     *  This is called by the "Load photo from library" button.
+     *  It calls up the photo chooser gallery.
+     *  The resulting file is sent to onActivityResult.
+     */
+    public void loadPhoto(View v) {
+        Intent picker = new Intent(Intent.ACTION_GET_CONTENT);
+        picker.setType("image/*");
+        startActivityForResult(picker,4);
+    }
+     
+    /*
+     * Called when the user picks a photo from the photo gallery.
+     * Passes the URL of the file to the GotPhoto activity.
+     * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Uri thePhoto = data.getData();
         Intent i = new Intent(GUI.this, GotPhoto.class);
+        i.putExtra("thePhoto", thePhoto);
         startActivity(i);
     }
-    
 }
