b83d4d5bd6adb40701535edef2b85f27b100dd5a
adriennefelt <adriennefelt@882c2ea8-2e7f-1d65-5f10-008cdebc5309>
2010-03-30 02:05:17 +0000

    added an onclick handler for loading a photo

--- MCR/src/edu/berkeley/sec/mcr/GUI.java
+++ MCR/src/edu/berkeley/sec/mcr/GUI.java
@@ -1,7 +1,9 @@
 package edu.berkeley.sec.mcr;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GUI extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +12,10 @@ public class GUI extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    public void sayHello(View v) {
+        Intent i = new Intent(GUI.this, GotPhoto.class);
+        startActivity(i);
+    }
+    
 }
