ecdb634c4e088d050421a9a5058d7a7e57553d4b
cksturton <cksturton@882c2ea8-2e7f-1d65-5f10-008cdebc5309>
2010-05-04 21:59:09 +0000

    Script info for Audiveris.

--- MCR/src/edu/berkeley/sec/mcr/GUI.java
+++ MCR/src/edu/berkeley/sec/mcr/GUI.java
@@ -1,43 +1,46 @@
 package edu.berkeley.sec.mcr;
 
-import java.io.FileNotFoundException;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 public class GUI extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    
-    /*
-     *  This is called by the "Load photo from library" button.
-     *  It calls up the photo chooser gallery.
-     *  The resulting file is sent to onActivityResult.
-     */
-    public void loadPhoto(View v) {
-        Intent picker = new Intent(Intent.ACTION_GET_CONTENT);
-        picker.setType("image/*");
-        startActivityForResult(picker,4);
-    }
-     
-    /*
-     * Called when the user picks a photo from the photo gallery.
-     * Passes the URL of the file to the GotPhoto activity.
-     * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
-     */
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Uri thePhoto = data.getData();
-        Intent i = new Intent(GUI.this, GotPhoto.class);
-        i.putExtra("thePhoto", thePhoto);
-        startActivity(i);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	static final int LOAD_PHOTO = 98;
+
+	/*
+	 * This is called by the "Load photo from library" button. It calls up the
+	 * photo chooser gallery. The resulting file is sent to onActivityResult.
+	 */
+	public void loadPhoto(View v) {
+		Intent picker = new Intent(Intent.ACTION_GET_CONTENT);
+		picker.setType("image/*");
+		startActivityForResult(picker, LOAD_PHOTO);
+	}
+
+	/*
+	 * Called when the user picks a photo from the photo gallery. Passes the URL
+	 * of the file to the GotPhoto activity.
+	 * 
+	 * @see android.app.Activity#onActivityResult(int, int,
+	 * android.content.Intent)
+	 */
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == LOAD_PHOTO) {
+			if (resultCode == RESULT_OK) {
+				Uri thePhoto = data.getData();
+				Intent i = new Intent(GUI.this, GotPhoto.class);
+				i.putExtra("thePhoto", thePhoto);
+				startActivity(i);
+			}
+		}
+	}
 }
--- MCR/src/edu/berkeley/sec/mcr/GotPhoto.java
+++ MCR/src/edu/berkeley/sec/mcr/GotPhoto.java
@@ -1,40 +1,65 @@
 package edu.berkeley.sec.mcr;
 
-import java.io.FileNotFoundException;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 /**
- * This Activity takes in a URI for a photo.
- * It displays the photo.
- * It has transform & play buttons.
+ * This Activity takes in a URI for a photo. It displays the photo. It has
+ * transform & play buttons.
  */
 public class GotPhoto extends Activity {
 
-    /*
-     * Expects an image Uri to be passed in as an extra, named "thePhoto".
-     */
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.gotphoto);
-        
-        // Get photo URI
-        Bundle extras = getIntent().getExtras();
-        Uri URI = (Uri) extras.get("thePhoto");
-        
-        // Set the photo
-        ImageView i = (ImageView) this.findViewById(R.id.sheet_music);
-        i.setImageURI(URI);
-        
-        // To get the actual image data:
-        // AssetFileDescriptor thePhoto = getContentResolver().openAssetFileDescriptor(URI, "r");
-    }
+	/*
+	 * Expects an image Uri to be passed in as an extra, named "thePhoto".
+	 */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gotphoto);
+
+		// Get photo URI
+		Bundle extras = getIntent().getExtras();
+		Uri URI = (Uri) extras.get("thePhoto");
+
+		// Set the photo
+		ImageView i = (ImageView) this.findViewById(R.id.sheet_music);
+		i.setImageURI(URI);
+
+		// To get the actual image data:
+		// AssetFileDescriptor thePhoto =
+		// getContentResolver().openAssetFileDescriptor(URI, "r");
+	}
+
+	static final int TRANS_LOCAL = 99;
+
+	// Called when Transform - Local button is clicked
+	public void startTransform(View v) {
+		// This does nothing. Wanted to make it look selected. Not important.
+		// ImageButton ib = (ImageButton)
+		// this.findViewById(R.id.transform_local);
+		// ib.setSelected(true);
+		Intent transform = new Intent(GotPhoto.this, TransformLocal.class);
+		// picker.setType("image/*");
+		startActivityForResult(transform, TRANS_LOCAL);
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == TRANS_LOCAL) {
+			if (resultCode == RESULT_OK) {
+				// Yay. Let user play music.
+				// Change the image to be the returned XML or marked-up sheet
+				// music.
+				Bundle extras = data.getExtras();
+				Uri musicXMLURI = (Uri) extras.get("musicXMLSheet");
+				ImageView i = (ImageView) this.findViewById(R.id.sheet_music);
+				i.setImageURI(musicXMLURI);
+			} else if (resultCode == RESULT_CANCELED) {
+				// Crap.
+			}
+		}
+	}
 
 }
