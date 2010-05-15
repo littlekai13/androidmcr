f8143e18750f41fce21ab5cda17b91f9ec3ba157
cksturton <cksturton@882c2ea8-2e7f-1d65-5f10-008cdebc5309>
2010-05-15 22:07:02 +0000

    resize imageview images to avoid out of memory error.

--- MCR/src/edu/berkeley/sec/mcr/GotPhoto.java
+++ MCR/src/edu/berkeley/sec/mcr/GotPhoto.java
@@ -3,13 +3,11 @@ package edu.berkeley.sec.mcr;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileDescriptor;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.channels.FileChannel;
@@ -23,9 +21,9 @@ import org.apache.http.util.ByteArrayBuffer;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -60,77 +58,113 @@ public class GotPhoto extends Activity {
 
 		// Set the photo
 		ImageView i = (ImageView) this.findViewById(R.id.sheet_music);
-		i.setImageURI(musicPhotoUri);
+
+		try {
+			InputStream is = getContentResolver()
+					.openInputStream(musicPhotoUri);
+
+			BitmapFactory.Options opts = new BitmapFactory.Options();
+			opts.inJustDecodeBounds = true;
+
+			Bitmap bmp = BitmapFactory.decodeStream(is, null, opts);
+
+			opts = new BitmapFactory.Options();
+			int scale = 10;
+			opts.inSampleSize = scale;
+			is = getContentResolver().openInputStream(musicPhotoUri);
+			bmp = BitmapFactory.decodeStream(is, null, opts);
+			i.setImageBitmap(bmp);
+
+		} catch (FileNotFoundException ex) {
+			// Should probably display some icon here.
+		}
+
 	}
 
 	static final int TRANS_LOCAL = 99;
 
 	// Called when Read button is clicked
 	public void startTransform(View v) {
-	    
-	    Thread t = new Thread() {
-	        public void run() {
-	            try {
-	                // Get the image
-	                AssetFileDescriptor thePhoto =
-	                    getContentResolver().openAssetFileDescriptor(musicPhotoUri, "r");
-	                FileChannel orig = thePhoto.createInputStream().getChannel();
-	                File imageFile = File.createTempFile("musicImages", ".jpg");
-	                FileChannel tmpFile = new FileOutputStream(imageFile).getChannel();      
-	                try { orig.transferTo(0, orig.size(), tmpFile); }
-	                catch (IOException e) { throw e; }
-	                finally { 
-	                    if (orig != null) orig.close();
-	                    if (tmpFile != null) tmpFile.close();
-	                }   
-	                // Send it
-	                HttpClient client = new DefaultHttpClient();
-	                if (imageFile.exists()) {
-	                    Log.v("Debug", "Image file exists");
-	                    Map postData = new HashMap();
-	                    Map postDataFiles = new HashMap();
-	                    postDataFiles.put("file", imageFile);
-	                    HttpData httpData = HttpRequest.postRequest(client, "http://gradgrind.erso.berkeley.edu:8080/Audiveris/RunAudiveris", postData, postDataFiles);
-	                    Log.v("Debug",httpData.data);
-	                } else {
-	                    Log.v("Debug","File not found "+ imageFile.getAbsolutePath());
-	                }
-	                
-	                // Download the MIDI file from the response
-	                URL url = new URL("http://gradgrind.erso.berkeley.edu/midi/example_1701664456.png.mid");
-	                File midiFile = new File(getFilesDir() + "/example_650415305.png.mid");
-	                URLConnection ucon = url.openConnection();
-	                InputStream is = ucon.getInputStream();
-	                BufferedInputStream bis = new BufferedInputStream(is);
-	                ByteArrayBuffer baf = new ByteArrayBuffer(50);
-	                int c = 0;
-	                while((c = bis.read()) != -1) {
-	                    baf.append((byte) c);
-	                }
-	                FileOutputStream fos = new FileOutputStream(midiFile);
-	                fos.write(baf.toByteArray());
-	                fos.close();
-	                midiFileUri = Uri.fromFile(midiFile);
-	                fd = openFileInput("example_650415305.png.mid").getFD();
-	            } catch (Exception e) {
-	                Log.v("Debug","EXCEPTION! " + e.getMessage());
-                    e.printStackTrace();
-                }
-	            Log.v("Debug","Done with worker thread");
-	            mHandler.post(mUpdateGUI);
-	        }
-	    };
-	    t.start();
+
+		Thread t = new Thread() {
+			public void run() {
+				try {
+					// Get the image
+					AssetFileDescriptor thePhoto = getContentResolver()
+							.openAssetFileDescriptor(musicPhotoUri, "r");
+					FileChannel orig = thePhoto.createInputStream()
+							.getChannel();
+					File imageFile = File.createTempFile("musicImages", ".jpg");
+					FileChannel tmpFile = new FileOutputStream(imageFile)
+							.getChannel();
+					try {
+						orig.transferTo(0, orig.size(), tmpFile);
+					} catch (IOException e) {
+						throw e;
+					} finally {
+						if (orig != null)
+							orig.close();
+						if (tmpFile != null)
+							tmpFile.close();
+					}
+					// Send it
+					HttpClient client = new DefaultHttpClient();
+					if (imageFile.exists()) {
+						Log.v("Debug", "Image file exists");
+						Map postData = new HashMap();
+						Map postDataFiles = new HashMap();
+						postDataFiles.put("file", imageFile);
+						HttpData httpData = HttpRequest
+								.postRequest(
+										client,
+										"http://gradgrind.erso.berkeley.edu:8080/Audiveris/RunAudiveris",
+										postData, postDataFiles);
+						Log.v("Debug", httpData.data);
+					} else {
+						Log.v("Debug", "File not found "
+								+ imageFile.getAbsolutePath());
+					}
+
+					// Download the MIDI file from the response
+					URL url = new URL(
+							"http://gradgrind.erso.berkeley.edu/midi/example_1701664456.png.mid");
+					File midiFile = new File(getFilesDir()
+							+ "/example_650415305.png.mid");
+					URLConnection ucon = url.openConnection();
+					InputStream is = ucon.getInputStream();
+					BufferedInputStream bis = new BufferedInputStream(is);
+					ByteArrayBuffer baf = new ByteArrayBuffer(50);
+					int c = 0;
+					while ((c = bis.read()) != -1) {
+						baf.append((byte) c);
+					}
+					FileOutputStream fos = new FileOutputStream(midiFile);
+					fos.write(baf.toByteArray());
+					fos.close();
+					midiFileUri = Uri.fromFile(midiFile);
+					fd = openFileInput("example_650415305.png.mid").getFD();
+				} catch (Exception e) {
+					Log.v("Debug", "EXCEPTION! " + e.getMessage());
+					e.printStackTrace();
+				}
+				Log.v("Debug", "Done with worker thread");
+				mHandler.post(mUpdateGUI);
+			}
+		};
+		t.start();
 	}
-	
+
 	// Threading for doing the HTTP POST in the background
-    final Handler mHandler = new Handler();
-    final Runnable mUpdateGUI = new Runnable() {
-        public void run() { updateGUI(); }
-    };
-    private void updateGUI() {
-        // update the UI
-    }
+	final Handler mHandler = new Handler();
+	final Runnable mUpdateGUI = new Runnable() {
+		public void run() {
+			updateGUI();
+		}
+	};
+
+	private void updateGUI() {
+		// update the UI
+	}
 
 	/*
 	 * This is called by the Play button. Plays the midi file returned by
@@ -143,26 +177,26 @@ public class GotPhoto extends Activity {
 		if (fd != null) {
 			MediaPlayer mp = new MediaPlayer();
 			try {
-                mp.setDataSource(fd);
-                mp.prepare();
-                mp.start();
-            } catch (IllegalArgumentException e) {
-                // TODO Auto-generated catch block
-                Log.v("Debug","Illegal argument exception");
-                e.printStackTrace();
-            } catch (IllegalStateException e) {
-                // TODO Auto-generated catch block
-                Log.v("Debug","Illegal state exception");
-                e.printStackTrace();
-            } catch (IOException e) {
-                Log.v("Debug","IOexception");
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (Exception e) {
-                Log.v("Debug","don't know why!?");
-                e.printStackTrace();
-            }
-            Log.v("Debug","Done playing");
+				mp.setDataSource(fd);
+				mp.prepare();
+				mp.start();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				Log.v("Debug", "Illegal argument exception");
+				e.printStackTrace();
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				Log.v("Debug", "Illegal state exception");
+				e.printStackTrace();
+			} catch (IOException e) {
+				Log.v("Debug", "IOexception");
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (Exception e) {
+				Log.v("Debug", "don't know why!?");
+				e.printStackTrace();
+			}
+			Log.v("Debug", "Done playing");
 		}
 
 		// No idea why the ACTION_VIEW works in GUI.java, but not here.
@@ -175,7 +209,7 @@ public class GotPhoto extends Activity {
 		// startActivity(playMusic);
 		else {
 			// What should we do if the user hasn't transformed anything yet?
-		    Log.v("Debug","SOMETHING WENT HORRIBLY WRONG");
+			Log.v("Debug", "SOMETHING WENT HORRIBLY WRONG");
 		}
 	}
 
