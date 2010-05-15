3f3370e43bc04206428b3975ab460bfa84df060a
cksturton <cksturton@882c2ea8-2e7f-1d65-5f10-008cdebc5309>
2010-05-15 22:07:02 +0000

    resize imageview images to avoid out of memory error.

--- trunk/MCR/src/edu/berkeley/sec/mcr/GUI.java
+++ trunk/MCR/src/edu/berkeley/sec/mcr/GUI.java
@@ -14,6 +14,7 @@ public class GUI extends Activity {
 		setContentView(R.layout.main);
 	}
 
+	static final int TRANSFORM = 97;
 	static final int LOAD_PHOTO = 98;
 	static final int BROWSE_MUSIC = 99;
 
@@ -27,6 +28,13 @@ public class GUI extends Activity {
 		startActivityForResult(picker, LOAD_PHOTO);
 	}
 
+	public void loadPhotoCallback(Intent data) {
+		Uri thePhoto = data.getData();
+		Intent transformer = new Intent(GUI.this, GotPhoto.class);
+		transformer.putExtra("thePhoto", thePhoto);
+		startActivityForResult(transformer, TRANSFORM);
+	}
+
 	/*
 	 * This is called by the "Saved music" button. It calls up the music chooser
 	 * gallery. The resulting file is sent to onActivity Result.
@@ -34,6 +42,8 @@ public class GUI extends Activity {
 	public void browseMusic(View v) {
 		Intent browser = new Intent(Intent.ACTION_GET_CONTENT);
 		browser.setType("audio/mid");
+		// When back button pressed, we should get RESULT_CANCELED
+		// result
 		startActivityForResult(browser, BROWSE_MUSIC);
 	}
 
@@ -47,14 +57,29 @@ public class GUI extends Activity {
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == LOAD_PHOTO) {
 			if (resultCode == RESULT_OK) {
-				Uri thePhoto = data.getData();
-				Intent i = new Intent(GUI.this, GotPhoto.class);
-				i.putExtra("thePhoto", thePhoto);
-				startActivity(i);
+				loadPhotoCallback(data);
+				/*
+				 * Uri thePhoto = data.getData(); Intent i = new
+				 * Intent(GUI.this, GotPhoto.class); i.putExtra("thePhoto",
+				 * thePhoto);
+				 * 
+				 * startActivityForResult(i, TRANSFORM);
+				 */
+			}
+		}
+
+		// Came back from GotPhoto.
+		else if (requestCode == TRANSFORM) {
+			if (resultCode == RESULT_OK) {
+				// I don't think we'll ever get here since we only return by
+				// pressing back button.
+			} else if (resultCode == RESULT_CANCELED) {
+				// Back button was pressed.
+
 			}
 		}
 		// Go to nice music player.
-		if (requestCode == BROWSE_MUSIC) {
+		else if (requestCode == BROWSE_MUSIC) {
 			if (resultCode == RESULT_OK) {
 				Uri midiFile = data.getData();
 				Intent i = new Intent(Intent.ACTION_VIEW);
