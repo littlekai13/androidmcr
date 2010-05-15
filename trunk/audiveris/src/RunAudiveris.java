9d61fa2a5053b8f2322fa3e711eae0c6071e1ed0
adriennefelt <adriennefelt@882c2ea8-2e7f-1d65-5f10-008cdebc5309>
2010-05-15 03:13:33 +0000

    got ss file uplaod working

--- trunk/audiveris/src/RunAudiveris.java
+++ trunk/audiveris/src/RunAudiveris.java
@@ -1,25 +1,43 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 public class RunAudiveris extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String serverPath = "/home/apf/androidmcr/audiveris/";
+	//private static final String serverPath = "/home/apf/androidmcr/audiveris/"; // SERVER
+	//private static final String uploadPath = "/home/apf/androidmcr/audiveris/uploads/"; // SERVER
+	private static final String serverPath = "/Users/apf/Documents/projects/music/repo/trunk/audiveris/";
+    private static final String uploadPath = "/Users/apf/Documents/projects/music/repo/trunk/audiveris/uploads/";
+    private String imageName;
        
     public RunAudiveris() {
         super();
+        imageName = serverPath + "example.png";
     }
     
     private void writeScript() {
-        String jpgPath = serverPath + "example.png";
+        String jpgPath = imageName;
         String midiPath = serverPath + "example.mid";
         String writeOut = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
             "<script sheet=\""+jpgPath+"\">\n" +
@@ -47,14 +65,6 @@ public class RunAudiveris extends HttpServlet {
     
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 	    // GET NOT SUPPORTED FOR MULTIPART FORMS
-	    
-	    // Write the run.script file
-        writeScript();
-        
-        // Execute Audiveris
-        executeAudiveris();
-        
-        // Set up the page
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
         out.println("<html><head>");
@@ -63,16 +73,66 @@ public class RunAudiveris extends HttpServlet {
                     "'Lucida Sans Unicode';font-size: 13px; }</style>");
         out.println("</head>");
         out.println("<body>");
-        
-        // File location
-        out.println("File is at: " + serverPath + "example.mid <br />");
-        
-        // Finish up
+        out.println("You need to POST an image to this form!  GET doesn't work. <br />");
         out.println("</body></html>");
         out.close();
 	}
+	
+	private void loadImage(HttpServletRequest request) {
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+        if (!isMultipart) {
+            System.err.println("File Not Uploaded");
+        } else {
+            FileItemFactory factory = new DiskFileItemFactory();
+            ServletFileUpload upload = new ServletFileUpload(factory);
+            List items = null;
+
+            try {
+                items = upload.parseRequest(request);
+            } catch (FileUploadException e) {
+                e.printStackTrace();
+            }
+            Iterator itr = items.iterator();
+            while (itr.hasNext()) {
+                FileItem item = (FileItem) itr.next();
+                if (item.isFormField()){
+                    String name = item.getFieldName();
+                    String value = item.getString();
+                } else {
+                    try {
+                        String itemName = item.getName();
+                        Random generator = new Random();
+                        int r = Math.abs(generator.nextInt());
+
+                        String reg = "[.*]";
+                        String replacingtext = "";
+                        Pattern pattern = Pattern.compile(reg);
+                        Matcher matcher = pattern.matcher(itemName);
+                        StringBuffer buffer = new StringBuffer();
+
+                        while (matcher.find()) {
+                            matcher.appendReplacement(buffer, replacingtext);
+                        }
+                        int IndexOf = itemName.indexOf("."); 
+                        String domainName = itemName.substring(IndexOf);
+                        String finalimage = buffer.toString()+"_"+r+domainName;
+
+                        File savedFile = new File(uploadPath+finalimage);
+                        item.write(savedFile);
+                        imageName = uploadPath+finalimage;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+	}
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	    
+	    // Handle the image upload
+	    loadImage(req);
+	    
 	    // Write the run.script file
         writeScript();
         
@@ -90,7 +150,7 @@ public class RunAudiveris extends HttpServlet {
         out.println("<body>"); 
         
         // File location
-        out.println("File is at: " + serverPath + "example.mid");
+        out.println("File is at: " + imageName);
         //out.println("HELLOOOO");
         
         // Finish up
